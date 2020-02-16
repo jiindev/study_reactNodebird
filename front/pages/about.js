56b893abf350059590c697aefe522ac2bb45268e
jiindev <jiindev@gmail.com>
2020-02-16 19:12:29 +0900

    next 라우팅 시스템

---
+++ front/pages/about.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+
+const About = () => {
+    return(
+        <>
+            <div>about me</div>
+        </>
+    )
+};
+
+export default About;
