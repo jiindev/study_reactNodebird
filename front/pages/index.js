409897744bef7d4e2217502beb410210af1e85cd
jiindev <jiindev@gmail.com>
2020-02-16 19:32:04 +0900

    ant design 적용

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,11 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import Link from 'next/link';
+import Head from 'next/head';
+import AppLayout from '../components/AppLayout';
 
 const Home = () => {
     return(
        <>
+       <Head>
+           <title>Nodebird</title>
+           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.9/antd.css"/>
+       </Head>
+        <AppLayout>
             <Link href="about"><a>about</a></Link>
             <div>Hello, next!</div>
+        </AppLayout>
         </>
     )
 };
