d3eae737393679c76fd5cf08c115ded14102591c
jiindev <jiindev@gmail.com>
2020-02-20 06:05:31 +0900

    회원가입 state와 커스텀훅

--- front/pages/signup.js
+++ front/pages/signup.js
@@ -4,31 +4,51 @@ import Head from 'next/head';
 import {Form, Input, Checkbox, Button} from 'antd';
 
 const Signup = () => {
-    const [id, setId] = useState('');
     const [nick, setNick] = useState('');
     const [password, setPassword] = useState('');
     const [passwordCheck, setPasswordCheck] = useState('');
     const [term, setTerm] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [termError, setTermError] = useState(false);
 
 
-    const onSubmit = () => {
-
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if(password !== passwordCheck){
+            return setPasswordError(true);
+        }
+        if(!term){
+            return setTermError(true);
+        }
+        console.log({
+            id, nick, password, passwordCheck, term
+        });
     }
-    const onChageId = (e) => {
-        setId(e.target.value);
-    }
-    const onChageNick = (e) => {
+    const onChangeNick = (e) => {
         setNick(e.target.value);
     }
-    const onChagePassword = (e) => {
+    const onChangePassword = (e) => {
         setPassword(e.target.value);
     }
     const onChangePasswordChk = (e) => {
+        setPasswordError(e.target.value !== password);
         setPasswordCheck(e.target.value);
     }
-    const onChageTerm = (e) => {
-        setTerm(e.target.value);
+    const onChangeTerm = (e) => {
+        setTermError(false);
+        setTerm(e.target.checked);
     }
+
+    const useInput = (initValue = null) => {
+        const [value, setter] = useState(initValue);
+        const handler = (e) => {
+            setter(e.target.value);
+        }
+        return [value, handler];
+    }
+
+    const [id, onChangeId] = useInput('');
+
     return (
         <>
         <Head>
@@ -40,25 +60,27 @@ const Signup = () => {
                 <div>
                     <label htmlFor="user-id">아이디</label>
                     <br/>
-                    <Input name="user-id" value={id} required onChange={onChageId}/>
+                    <Input name="user-id" value={id} required onChange={onChangeId}/>
                 </div>
                 <div>
                     <label htmlFor="user-nick">닉네임</label>
                     <br/>
-                    <Input name="user-nick" value={nick} required onChange={onChageNick}/>
+                    <Input name="user-nick" value={nick} required onChange={onChangeNick}/>
                 </div>
                 <div>
                     <label htmlFor="user-password">비밀번호</label>
                     <br/>
-                    <Input name="user-password" value={password} type="password" required onChange={onChagePassword}/>
+                    <Input name="user-password" value={password} type="password" required onChange={onChangePassword}/>
                 </div>
                 <div>
                     <label htmlFor="user-password-chk">비밀번호 체크</label>
                     <br/>
                     <Input name="user-password-check" value={passwordCheck} type="password" required onChange={onChangePasswordChk}/>
+                    {passwordError && <div style={{color:'red'}}>비밀번호가 일치하지 않습니다.</div>}
                 </div>
-                <div>
-                    <Checkbox name="user-term" value={term} onChange={onChageTerm}>이에 동의합니다.</Checkbox>
+                <div style={{marginTop:'10'}}>
+                    <Checkbox name="user-term" value={term} onChange={onChangeTerm}>이에 동의합니다.</Checkbox>
+                    {termError && <div style={{color:'red'}}>약관에 동의하세요</div>}
                 </div>
                 <div>
                     <Button type="primary" htmlType="submit">가입하기</Button>
