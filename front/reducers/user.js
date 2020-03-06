5dd886d947365d0977f0a0f5417421265efa158c
jiindev <jiindev@gmail.com>
2020-03-06 17:05:27 +0900

    리듀서 여러 개 합치기

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -1,4 +1,4 @@
-const initialState = {
+export const initialState = {
     isLoggedIn : false,
     user: {},
 };
@@ -34,3 +34,5 @@ const reducer = (state = initialState, action) => {
         }
     }
 };
+
+export default reducer;
