0c181303a1c4504f0b24e2b8dbfb92849e8a94c9
jiindev <jiindev@gmail.com>
2020-03-17 23:52:12 +0900

    리덕스 사가 맛보기

--- front/reducers/user.js
+++ front/reducers/user.js
@@ -13,13 +13,19 @@ export const initialState = {
 
 export const SIGN_UP = 'SIGN_UP';
 export const LOG_IN = 'LOG_IN'; // 액션의 이름
+export const LOG_IN_SUCCESS = 'LOG_IN_SUCCESS';
+export const LOG_IN_FAILURE = 'LOG_IN_FAILURE';
 export const LOG_OUT = 'LOG_OUT'
+export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 
 export const signUpAction = (data) => {
     return {
         type: SIGN_UP,
         data: data
     }
+}
+export const signUpSuccess = {
+    type:SIGN_UP_SUCCESS,
 }
 export const loginAction = {
     type: LOG_IN,
