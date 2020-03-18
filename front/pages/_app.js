d86e224072786ffb590d26e25a6f4fd3fd567c8e
jiindev <jiindev@gmail.com>
2020-03-18 23:16:05 +0900

    사가 미들웨어 리덕스에 연결하기

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -4,8 +4,11 @@ import propTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
 import AppLayout from '../components/AppLayout';
 import {createStore, compose, applyMiddleware} from 'redux';
-import {Provider} from 'react-redux';
+import {Provider, connect} from 'react-redux';
 import reducer from '../reducers';
+import createSagaMiddleware from 'redux-saga'; 
+import rootsaga from '../sagas';
+
 // import { initialState } from '../reducers/user';
 
 const NodeBird = ({Component, store}) => {
@@ -29,11 +32,22 @@ NodeBird.propTypes = {
     store: propTypes.object
 };
 
+const middleware = (store)=>(next)=>(action)=>{
+    console.log(action) //다른 작업들
+    next(action);
+};
+
+
+
 export default withRedux((initialState, options)=>{
-    const middlewares = [];
-    const enhancer = compose(applyMiddleware(...middlewares),
+    const sagaMiddleware = createSagaMiddleware();
+    const middlewares = [sagaMiddleware];
+    const enhancer = process.env.NODE_ENV === 'production' ?
+    compose(applyMiddleware(...middlewares))
+    :compose(applyMiddleware(...middlewares),
         !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
     );
     const store = createStore(reducer, initialState, enhancer);
+    sagaMiddleware.run(rootsaga);
     return store;
 })(NodeBird);
