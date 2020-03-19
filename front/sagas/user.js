06d1c621917be0e14a582daa0e06974c56d8e571
jiindev <jiindev@gmail.com>
2020-03-20 04:38:31 +0900

    fork, call, 사가 총정리

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -8,7 +8,7 @@ function loginAPI(){
 }
 function* login(){
     try{
-        yield call(loginAPI);  //함수 동기적 호출
+        yield call(loginAPI);  //함수 동기적 호출 (응답을 받을 때까지 기다림)
         yield put({ //put은 dispatch와 동일
             type: LOG_IN_SUCCESS
         })
@@ -21,14 +21,14 @@ function* login(){
 
 }
 function* watchLogin(){
-    while(true){
-        yield take(LOG_IN);
-        yield delay(2000);
-        yield put({
-            type:LOG_IN_SUCCESS,
-        })
-    }
-    
+    yield takeEvery(LOG_IN, login);
+}
+
+function* hello(){
+    yield delay(1000);
+    yield put({
+        type:'BYE_SAGA' //액션을 여러번 동시에 요청할 경우 마지막꺼만 처리
+    })
 }
 
 function* watchHello(){
@@ -37,12 +37,7 @@ function* watchHello(){
     //         type:'BYE_SAGA'
     //     })
     // });
-    yield takeLatest(HELLO_SAGA, function*(){
-        yield delay(1000);
-        yield put({
-            type:'BYE_SAGA'
-        })
-    });
+    yield takeLatest(HELLO_SAGA, hello);
 }
 
 // function* watchHello(){
@@ -62,8 +57,8 @@ function* watchSignUp(){
 
 export default function* userSaga(){
     yield all([
-        watchHello(),
-        watchLogin(),
-        watchSignUp(),
+        fork(watchHello), //비동기호출
+        fork(watchSignUp),
+        fork(watchLogin),
     ]);
 }
