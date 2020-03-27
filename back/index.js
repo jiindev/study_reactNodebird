5741e558fc42e42e772778be7fc13694f7aaf36e
jiindev <jiindev@gmail.com>
2020-03-27 11:13:07 +0900

    회원가입 컨트롤러 만들기

--- back/index.js
+++ back/index.js
@@ -6,6 +6,9 @@ const postsAPIRouter = require('./routes/posts');
 const app = express();
 db.sequelize.sync();
 
+app.use(express.json()); // json 형식의 본문 처리
+app.use(express.urlencoded({extended: true})); // form으로 넘어온 데이터 처리
+
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter); 
 app.use('/api/post', postAPIRouter); 
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../models');
+const bcrypt = require('bcrypt');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res) => { // /api/user/
 
 });
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res, next) => { // POST /api/user 회원가입
+    try{
+        const exUser = await db.User.findOne({
+            where: {
+                userId: req.body.userId,
+            },
+        });
+        if(exUser){
+            return res.status(403).send('이미 사용중인 아이디입니다.');
+        }
+        const hashedPassword = await bcrypt.hash(req.body.password, 12); // salt는 10~13 사이로
+        const newUser = await db.User.create({
+            nicknam: req.body.nicknam,
+            userId: req.body.userId,
+            password: hashedPassword,
+        });
+        console.log(newUser);
+        return res.status(200).json(newUser);
+    }catch (e) {
+        console.error(e);
+        return next(e);
+    }
 });
 router.get('/:id', (req, res) => { // 남의 정보 가져오기 ex) /3
 
