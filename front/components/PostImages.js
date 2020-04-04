f9f48b7fea580ddd6072dbb2698ec931cda470dd
jiindev <jiindev@gmail.com>
2020-04-04 12:43:24 +0900

    react-slick으로 이미지 슬라이더 구현

--- front/components/PostImages.js
+++ front/components/PostImages.js
@@ -1,30 +1,51 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Icon} from 'antd';
+import ImagesZoom from './ImagesZoom';
 
 const PostImages = ({images}) => {
+    const [showImageZoom, setShowImageZoom] = useState(false);
+    const onZoom = useCallback(
+        () => {
+            setShowImageZoom(true);
+        },
+        [],
+    )
+    const onClose = useCallback(()=>{
+        setShowImageZoom(false);
+    }, []);
+
     if(images.length===1){
         return (
-            <img src={`http://localhost:3065/${images[0].src}`}/>
+            <>
+                <img src={`http://localhost:3065/${images[0].src}`} onClick={onZoom}/>
+                {showImageZoom && <ImagesZoom images={images} onClose={onClose}/>}
+            </>
         );
     }
     if(images.length===2){
         return(
-            <div>
-                 <img src={`http://localhost:3065/${images[0].src}`} width='50%'/>
-                 <img src={`http://localhost:3065/${images[1].src}`} width='50%'/>
-            </div>
+            <>
+                <div>
+                    <img src={`http://localhost:3065/${images[0].src}`} width='50%' onClick={onZoom}/>
+                    <img src={`http://localhost:3065/${images[1].src}`} width='50%' onClick={onZoom}/>
+                </div>
+                {showImageZoom && <ImagesZoom images={images} onClose={onClose}/>}
+            </>
         );
     }
     return (
-        <div>
-            <img src={`http://localhost:3065/${images[0].src}`} width='50%'/>
-            <div style={{display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign:'middle'}}>
-                <Icon type="plus"/>
-                <br/>
-                {images.length -1} 개의 사진 더 보기
+        <>
+            <div>
+                <img src={`http://localhost:3065/${images[0].src}`} width='50%' onClick={onZoom}/>
+                <div style={{display: 'inline-block', width: '50%', textAlign: 'center', verticalAlign:'middle'}} onClick={onZoom}>
+                    <Icon type="plus"/>
+                    <br/>
+                    {images.length -1} 개의 사진 더 보기
+                </div>
             </div>
-        </div>
+            {showImageZoom && <ImagesZoom images={images} onClose={onClose}/>}
+        </>
     );
 
     
--- front/pages/_app.js
+++ front/pages/_app.js
@@ -19,6 +19,8 @@ const NodeBird = ({ Component, store, pageProps }) => (
         <title>Nodebird</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.9/antd.css" />
         <link rel="icon" href="data:;base64,iVBORw0KGgo="></link>
+        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />  
       </Head>
       <AppLayout>
         <Component {...pageProps} />
