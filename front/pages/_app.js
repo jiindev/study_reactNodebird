4c9df8db09aa854ccb88fa77a06ba791f240cd4a
jiindev <jiindev@gmail.com>
2020-04-10 23:51:56 +0900

    서버 사이드 렌더링(SSR)

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -1,14 +1,14 @@
-
-import React from 'react';
-import Head from 'next/head';
-import propTypes from 'prop-types';
-import withRedux from 'next-redux-wrapper';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import createSagaMiddleware from 'redux-saga';
-import reducer from '../reducers';
-import AppLayout from '../components/AppLayout';
-import rootsaga from '../sagas';
+import React from "react";
+import Head from "next/head";
+import propTypes from "prop-types";
+import withRedux from "next-redux-wrapper";
+import withReduxSaga from "next-redux-saga";
+import { createStore, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import createSagaMiddleware from "redux-saga";
+import reducer from "../reducers";
+import AppLayout from "../components/AppLayout";
+import rootsaga from "../sagas";
 
 // import { initialState } from '../reducers/user';
 
@@ -17,10 +17,22 @@ const NodeBird = ({ Component, store, pageProps }) => (
     <Provider store={store}>
       <Head>
         <title>Nodebird</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.9/antd.css" />
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.26.9/antd.css"
+        />
         <link rel="icon" href="data:;base64,iVBORw0KGgo="></link>
-        <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />  
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
       </Head>
       <AppLayout>
         <Component {...pageProps} />
@@ -32,7 +44,7 @@ const NodeBird = ({ Component, store, pageProps }) => (
 NodeBird.propTypes = {
   Component: propTypes.elementType.isRequired,
   store: propTypes.object.isRequired,
-  pageProps:propTypes.object.isRequired
+  pageProps: propTypes.object.isRequired,
 };
 
 NodeBird.getInitialProps = async (context) => {
@@ -48,13 +60,19 @@ NodeBird.getInitialProps = async (context) => {
 const configureStore = (initialState, options) => {
   const sagaMiddleware = createSagaMiddleware();
   const middlewares = [sagaMiddleware];
-  const enhancer = process.env.NODE_ENV === 'production'
-    ? compose(applyMiddleware(...middlewares))
-    : compose(applyMiddleware(...middlewares),
-      !options.isServer && window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f);
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : compose(
+          applyMiddleware(...middlewares),
+          !options.isServer &&
+            window.__REDUX_DEVTOOLS_EXTENSION__ !== "undefined"
+            ? window.__REDUX_DEVTOOLS_EXTENSION__()
+            : (f) => f
+        );
   const store = createStore(reducer, initialState, enhancer);
-  sagaMiddleware.run(rootsaga);
+  store.sagaTask = sagaMiddleware.run(rootsaga);
   return store;
 };
 
-export default withRedux(configureStore)(NodeBird);
+export default withRedux(configureStore)(withReduxSaga(NodeBird));
--- front/pages/index.js
+++ front/pages/index.js
@@ -1,33 +1,31 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import PostForm from '../components/PostForm';
-import PostCard from '../components/PostCard';
-import {loginAction, logoutAction} from '../reducers/user';
-import { LOAD_MAIN_POSTS_REQUEST } from '../reducers/post';
-
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PostForm from "../components/PostForm";
+import PostCard from "../components/PostCard";
+import { loginAction, logoutAction } from "../reducers/user";
+import { LOAD_MAIN_POSTS_REQUEST } from "../reducers/post";
 
 const Home = () => {
-    const dispatch = useDispatch();
-    const {user, me} = useSelector(state=>state.user);
-    const {mainPosts} = useSelector(state=>state.post);
-    useEffect(() => {
-        dispatch({
-            type: LOAD_MAIN_POSTS_REQUEST
-        });
-    }, []);
+  const dispatch = useDispatch();
+  const { user, me } = useSelector((state) => state.user);
+  const { mainPosts } = useSelector((state) => state.post);
 
-    return(
-       <>
-            <div>
-                {me && <PostForm/>}
-                {mainPosts.map((c)=>{
-                    return(
-                        <PostCard key={c} post={c}/>
-                    );
-                })}
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div>
+        {me && <PostForm />}
+        {mainPosts.map((c) => {
+          return <PostCard key={c} post={c} />;
+        })}
+      </div>
+    </>
+  );
+};
+
+Home.getInitialProps = async (context) => {
+  context.store.dispatch({
+    type: LOAD_MAIN_POSTS_REQUEST,
+  });
 };
 
 export default Home;
