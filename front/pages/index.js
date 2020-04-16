d33227f0fc183df6078df50ffa1ee433557b532c
jiindev <jiindev@gmail.com>
2020-04-16 12:48:47 +0900

    프론트 단에서 리덕스 액션 호출 막기

--- front/pages/index.js
+++ front/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useCallback} from "react";
+import React, { useEffect, useCallback, useRef} from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PostForm from "../components/PostForm";
 import PostCard from "../components/PostCard";
@@ -9,14 +9,19 @@ const Home = () => {
   const dispatch = useDispatch();
   const { user, me } = useSelector((state) => state.user);
   const { mainPosts, hasMorePost } = useSelector((state) => state.post);
+  const countRef = useRef([]);
 
   const onScroll = useCallback(() => {
     if(window.scrollY + document.documentElement.clientHeight > document.documentElement.scrollHeight - 300){
       if (hasMorePost){
-        dispatch({
-          type: LOAD_MAIN_POSTS_REQUEST,
-          lastId: mainPosts[mainPosts.length-1].id,
-        })
+        const lastId = mainPosts[mainPosts.length-1].id;
+        if(!countRef.current.includes(lastId)){
+          dispatch({
+            type: LOAD_MAIN_POSTS_REQUEST,
+            lastId
+          });
+          countRef.current.push(lastId);
+        }
       }
     }
   }, [hasMorePost, mainPosts.length]);
